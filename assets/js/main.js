ed911d9dc622fd77cb929dc8c00bd2464c0b4194
Pitt009 <gmartinez28837@gmail.com>
2017-06-05 16:41:30 -0500

    Mapa con ubicacion del usuario

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,43 +1,58 @@
 
-var map;
-  function initMap(){
-    var laboratoriaMexico = {lat: 19.4175048, lng: -99.1652178,};
+var obtenerUbicacion = function (e) {
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(mostrarPosicion);
+	} else {
+		alert("Actualice su navegador");
+	}
+};
 
-    var map = new google.maps.Map(document.getElementById('map'),{
-    zoom: 18,
-    center:laboratoriaMexico
+var mostrarPosicion = function (posicion) {
+	var coordenadas = {
+		lat: posicion.coords.latitude,
+		lng: posicion.coords.longitude
+	};
+	mostrarMapa(coordenadas);
+};
 
+var mostrarMapa = function (coordenadas) {
+	var map = new google.maps.Map($('.map')[0], {
+      zoom: 17,
+      center: coordenadas
     });
     var marker = new google.maps.Marker({
-    position:laboratoriaMexico,
-    map: map
-  });
-
+      position: coordenadas,
+      map: map
+    });
 }
 
 var restaurantes = [
  {
     "nombre": "Rosseta",
     "comida": "Internacional",
+    "foto" : "http://via.placeholder.com/150x150",
     "latitud": "19.4175048",
-    "longitud": "-99.1652178",
+    "longitud": "-99.1652178"
  },
  {
    "nombre": "Macelleria Roma",
    "comida": "Pizzas y platillos Italianos",
+   "foto" : "http://via.placeholder.com/150x150",
    "latitud": "19.4175048",
-   "longitud": "-99.1652178",
+   "longitud": "-99.1652178"
  },
  {
    "nombre": "Fonda Fina",
    "comida": "Antojitos y comida mexicana",
+   "foto" : "http://via.placeholder.com/150x150",
    "latitud": "19.4175048",
-   "longitud": "-99.1652178",
+   "longitud": "-99.1652178"
  },
  {
    "nombre": "Moshi Moshi",
    "comida": "Sushi",
+   "foto" : "http://via.placeholder.com/150x150",
    "latitud": "19.4175046",
-   "longitud": "-99.1717839",
+   "longitud": "-99.1717839"
  },
 ];
